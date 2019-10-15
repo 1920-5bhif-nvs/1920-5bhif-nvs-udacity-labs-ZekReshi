0418428338f2bfe93a17b3e1a710a2ac8d5215c5
Schwarcz Florian <florian.schwarcz@gmx.at>
2019-10-15 09:37:25 +0200

    Added data binding and migrated to androidx

--- AboutMe/app/src/main/java/at/htl/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/at/htl/aboutme/MainActivity.kt
@@ -1,12 +1,40 @@
 package at.htl.aboutme
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import at.htl.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Florian Schwarcz")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            doneButton.visibility = View.GONE
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
