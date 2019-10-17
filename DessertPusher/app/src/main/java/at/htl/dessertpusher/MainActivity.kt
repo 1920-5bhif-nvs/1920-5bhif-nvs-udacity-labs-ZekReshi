371ed1cd43b214b5b7059953fac2bd28d1208490
Schwarcz Florian <florian.schwarcz@gmx.at>
2019-10-17 15:55:00 +0200

    Added lifecycle annotations

--- DessertPusher/app/src/main/java/at/htl/dessertpusher/MainActivity.kt
+++ DessertPusher/app/src/main/java/at/htl/dessertpusher/MainActivity.kt
@@ -84,7 +84,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
 
-        dessertTimer = DessertTimer();
+        dessertTimer = DessertTimer(this.lifecycle)
     }
 
     /**
@@ -162,7 +162,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStart() {
         super.onStart()
         Timber.i("onStart called")
-        dessertTimer.startTimer()
     }
 
     override fun onResume() {
@@ -183,7 +182,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStop() {
         super.onStop()
         Timber.i("onStop called")
-        dessertTimer.stopTimer()
     }
 
 }
