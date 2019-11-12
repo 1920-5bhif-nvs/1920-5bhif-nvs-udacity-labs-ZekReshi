0c5719d9387cdd4ec990f1dc762a0c9c7bb8a215
Schwarcz Florian <florian.schwarcz@gmx.at>
2019-11-12 11:34:34 +0100

    Added button enable/disable

--- SleepTracker/app/src/main/java/at/htl/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ SleepTracker/app/src/main/java/at/htl/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -46,6 +46,18 @@ class SleepTrackerViewModel(
         formatNights(nights, application.resources)
     }
 
+    val startButtonVisible = Transformations.map(tonight) {
+        null == it
+    }
+
+    val stopButtonVisible = Transformations.map(tonight) {
+        null != it
+    }
+
+    val clearButtonVisible = Transformations.map(nights) {
+        it?.isNotEmpty()
+    }
+
     init {
         initializeTonight()
     }
