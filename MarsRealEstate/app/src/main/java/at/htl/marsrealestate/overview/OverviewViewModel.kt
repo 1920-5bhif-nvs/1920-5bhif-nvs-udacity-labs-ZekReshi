9e5432df0ce58a43d850120af75abb0f06fdb356
Schwarcz Florian <florian.schwarcz@gmx.at>
2019-11-15 11:24:31 +0100

    Added filter

--- MarsRealEstate/app/src/main/java/at/htl/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate/app/src/main/java/at/htl/marsrealestate/overview/OverviewViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import at.htl.marsrealestate.network.MarsApi
+import at.htl.marsrealestate.network.MarsApiFilter
 import at.htl.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.*
 import java.lang.Exception
@@ -54,15 +55,15 @@ class OverviewViewModel : ViewModel() {
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
-        getMarsRealEstateProperties()
+        getMarsRealEstateProperties(MarsApiFilter.SHOW_ALL)
     }
 
     /**
      * Sets the value of the status LiveData to the Mars API status.
      */
-    private fun getMarsRealEstateProperties() {
+    private fun getMarsRealEstateProperties(filter: MarsApiFilter) {
         coroutineScope.launch {
-            var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
+            var getPropertiesDeferred = MarsApi.retrofitService.getProperties(filter.value)
             try {
                 _status.value = MarsApiStatus.LOADING
 
@@ -75,6 +76,10 @@ class OverviewViewModel : ViewModel() {
                 _properties.value = ArrayList()
             }
         }
+    }
+
+    fun updateFilter(filter: MarsApiFilter) {
+        getMarsRealEstateProperties(filter)
     }
 
     fun displayPropertyDetails(marsProperty: MarsProperty) {
